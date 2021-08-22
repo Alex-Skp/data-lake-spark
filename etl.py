89d119146c333d5ff3dba1c02f437b83480a65a4
Alex-Skp <desscallion@gmail.com>
2021-08-23 00:03:03 +0200

    to test in AWS

--- etl.py
+++ etl.py
@@ -24,8 +24,8 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = "./data/song_data/*/*/*/*.json"
-    
+    song_data = input_data + "data/song_data/*/*/*/*.json"
+     
     # read song data file
     song_data_schema = StructType([
     StructField("num_songs",         IntegerType()),
@@ -64,12 +64,12 @@ def process_song_data(spark, input_data, output_data):
     # write artists table to parquet files
     artists_table.write.mode('overwrite')\
         .partitionBy("artist_id")\
-        .parquet("./output/artists_table.parquet")
+        .parquet(output_data + "output/artists_table.parquet")
 
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data = "./data/log_data"
+    log_data = input_data + "data/log_data"
 
     # read log data file
     df = spark.read.json(log_data)
@@ -87,9 +87,10 @@ def process_log_data(spark, input_data, output_data):
     # write users table to parquet files
     artists_table.write.mode('overwrite')\
         .partitionBy("user_id")\
-        .parquet("./output/users_table.parquet")
+        .parquet(output_data + "output/users_table.parquet")
 
     # create timestamp column from original timestamp column
+    df = df.na.drop(subset=["ts"])
     df = df.withcolumn("timestamp", from_unixtime(col("ts")/1000))
     
     # extract columns to create time table
@@ -105,23 +106,49 @@ def process_log_data(spark, input_data, output_data):
     # write time table to parquet files partitioned by year and month
     time_table.mode('overwrite')\
         .partitionBy("year", "month")\
-        .parquet("./output/time_table.parquet")
+        .parquet(output_data + "output/time_table.parquet")
 
     # read in song data to use for songplays table
 
-    song_df = 
+    song_data_url = input_data + "data/song_data/*/*/*/*.json"
+    song_data_schema = StructType([
+    StructField("num_songs",         IntegerType()),
+    StructField("artist_id",         StringType()),
+    StructField("artist_latitude",   FloatType()),
+    StructField("artist_longitude",  FloatType()),
+    StructField("artist_location",   StringType()),
+    StructField("artist_name",       StringType()),
+    StructField("song_id",           StringType()),
+    StructField("title",             StringType()),
+    StructField("duration",          FloatType()),
+    StructField("year",              IntegerType()),
+    ])
+    song_data = spark.read.json(song_data_url, schema=song_data_schema) 
 
     # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
+    songplays_table = df.join(song_data, (song_data.title == df.song)\
+                                        &(song_data.artist_name == df.artist)\
+                                        &(song_data.duration == df.length), how='left')\
+                        .select(col("timestamp").alias("start_time"),
+                                col("userId").alias("user_id"),
+                                col("level"),
+                                col("song_id"),
+                                col("artist_id"),
+                                col("sessionId").alias("session_id"),
+                                col("location"),
+                                col("userAgent").alias("user_agent")).distinct()
 
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
+
+    # write songplays table to parquet files partitioned by year and month??
+    songplays_table.mode('overwrite')\
+        .partitionBy("year", "month")\
+        .parquet(output_data + "output/songplays_table.parquet")
 
 
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    output_data = "" #INSERT BUCKET URL FOR THE OUTPUT 
     
     process_song_data(spark, input_data, output_data)    
     process_log_data(spark, input_data, output_data)
